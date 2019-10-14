0e2b361312f743f5ecf8e76c9e57ee6949473d4f
DESKTOP-999UR4G\Nam <nam.trantrung@vietstarexpress.com>
2019-10-14 18:00:18 +0700

    Add initial project

---
+++ Receive/Receive.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "10.0.1.222", Port = 5672, UserName = "admin", Password = "admin", VirtualHost = "/" };
+            // MethodReceive1(args, factory);
+            MethodReceiveWithTopic(args, factory);
+
+            //
+
+        }
+
+        public static void MethodReceive1(string[] args, ConnectionFactory factory)
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                                                     durable: true,
+                                                     exclusive: false,
+                                                     autoDelete: false,
+                                                     arguments: null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume(queue: "task_queue",
+                                     autoAck: false,
+                                     consumer: consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        public static void MethodReceiveWithTopic(string[] args, ConnectionFactory factory)
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+                var queueName = channel.QueueDeclare().QueueName;
+                Console.WriteLine(queueName);
+
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} [binding_key...]",
+                                            Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                foreach (var bindingKey in args)
+                {
+                    channel.QueueBind(queue: queueName,
+                                      exchange: "topic_logs",
+                                      routingKey: bindingKey);
+                }
+
+                Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                      routingKey,
+                                      message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Send
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "10.0.1.222", Port = 5672, UserName = "admin", Password = "admin", VirtualHost = "/" };
+
+
+            MethodSendWithTopic(args, factory);
+            // MethodSend1(args, factory);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+
+
+        public static void MethodSendWithTopic(string[] args, ConnectionFactory factory)
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs",
+                                       type: "topic");
+
+                var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+                var message = (args.Length > 1)
+                              ? string.Join(" ", args.Skip(1).ToArray())
+                              : "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "topic_logs",
+                                     routingKey: routingKey,
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+            }
+        }
+
+        public static void MethodSend1(string[] args, ConnectionFactory factory)
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "task_queue",
+                                     basicProperties: properties,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+    }
+}
