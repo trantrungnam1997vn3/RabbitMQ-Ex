dfcb6ade5c9aebf17094e112b8ebb20e61bf8200
DESKTOP-999UR4G\Nam <nam.trantrung@vietstarexpress.com>
2019-10-16 19:37:20 +0700

    complete handle get message

--- RPCServer/RPCServer.cs
+++ RPCServer/RPCServer.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel;
-using System;
+﻿using System;
+using System.Linq;
 using System.Text;
+
 using RabbitMQ.Client;
+
 using RabbitMQ.Client.Events;
 
 namespace RPCServer
@@ -10,14 +12,33 @@ namespace RPCServer
 
     class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "10.0.1.222", Port = 5672, UserName = "admin", Password = "admin", VirtualHost = "/" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "rpc_queue/uuid", durable: false,
-                  exclusive: false, autoDelete: false, arguments: null);
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+                // var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+                // var message = (args.Length > 1)
+                //               ? string.Join(" ", args.Skip(1).ToArray())
+                //               : "Hello World!";
+
+                // var body = Encoding.UTF8.GetBytes(message);
+
+                foreach (var bindingKey in args)
+                {
+                    channel.QueueBind(queue: "rpc_queue",
+                                      exchange: "topic_logs",
+                                      routingKey: bindingKey
+                  );
+                }
+
+                Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+
+                // channel.QueueDeclare(queue: "rpc_queue", durable: false,
+                //   exclusive: false, autoDelete: false, arguments: null);
                 channel.BasicQos(0, 1, false);
                 // channel.BasicQos(10);
                 var consumer = new EventingBasicConsumer(channel);
@@ -71,7 +92,8 @@ namespace RPCServer
             return fib(n - 1) + fib(n - 2);
         }
 
-        private static String getHello(String name) {
+        private static String getHello(String name)
+        {
             return "Hello" + name;
         }
     }
